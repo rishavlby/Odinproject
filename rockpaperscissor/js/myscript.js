bd18ffbc58dcdb0b64850f154d7c8fcd89c41f92
Rishav Arora <rishavarora@yahoo.com>
2025-01-20 19:37:22 -0800

    updated code to use classes

--- rockpaperscissor/js/myscript.js
+++ rockpaperscissor/js/myscript.js
@@ -1,111 +1,100 @@
-const rck = document.querySelector("#Rock");
-const papr = document.querySelector("#Paper");
-const scissr = document.querySelector("#Scissor");
+class game{
+constructor(){
 
-let humanScore=0;
-let computerScore=0;
-
-rck.addEventListener('click', () => {
-playRound('rock', getComputerChoice());
-});
-
-papr.addEventListener('click', () => {
-playRound('paper', getComputerChoice());
-});
-scissr.addEventListener('click', () => {
-playRound('scissor', getComputerChoice());
-});
-
-//function for computer to choose Random things
-function getComputerChoice()
-{
-let things = ['rock', 'paper', 'scissor'];
-let computerChoice = things[Math.floor(Math.random()*things.length)];
-// alert('The computer chose: ' + computerChoice);
-return computerChoice;
+    this.humanScore = 0;
+    this.computerScore = 0;
+    this.choice = ['rock', 'paper','scissor'];
+    this.addEventListeners();
 }
 
-function displayMessage(message)
-{ 
-const res = document.getElementById("results");
-res.textContent= message;
-}
-function resultMessage(messager)
-{ 
-const resmess = document.getElementById("result2");
-resmess.textContent= messager;
-}
-function finalresult(finres)
-{
-const rem = document.getElementById("winner");
-rem.textContent =finres;
+addEventListeners() {
+    document.querySelector("#Rock").addEventListener('click', () => {
+        this.playRound('rock', this.getComputerChoice()); 
+    })
+        
+    document.querySelector("#Paper").addEventListener('click', () => {
+        this.playRound('paper', this.getComputerChoice());
+    }); 
+    document.querySelector("#Scissor").addEventListener('click', () => {
+        this.playRound('scissor', this.getComputerChoice());
+    }); 
+  }
 
-setTimeout(() => {
-rem.style.display = 'none';},800);
+  getComputerChoice() {
+    return this.choice[Math.floor(Math.random()*this.choice.length)];
+        }
 
+   displayMessage(message){
+    document.getElementById("results").textContent =message;
+    }
+
+   resultMessage(){ 
+    return document.getElementById("result2").textContent = `Current Score - Human: ${this.humanScore}, Computer: ${this.computerScore}`;
+      }
+
+   finalresult(finres){
+       const rem = document.getElementById("winner");
+    rem.textContent =finres;
+//Timeout for message
+    setTimeout(() => {
+    rem.style.display = 'none';},800);
 }; 
 
-function resetgame()
-{
-humanScore = 0;
-computerScore = 0;
+resetgame(){
+this.humanScore = 0;
+this.computerScore = 0;
 }
-function playRound(humanChoice,computerChoice)
-{
+
+playRound(humanChoice,computerChoice){
+
 if(humanChoice == 'rock' && computerChoice == 'scissor') 
 { 
-displayMessage("You Win! Rock beats Scissor."); 
-//console.log("You Win! Rock beats Scissor.")
-humanScore++; 
+this.displayMessage("You Win! Rock beats Scissor."); 
+this.humanScore++; 
 } 
 else if(humanChoice == 'scissor' && computerChoice == 'paper'){
-displayMessage("You Win! Scissor beats Paper.");
-// console.log("You Win! Scissor beats Paper.")
-humanScore++;
+this.displayMessage("You Win! Scissor beats Paper.");
+this.humanScore++;
 }
 else if(humanChoice == 'paper' && computerChoice == 'rock'){
-displayMessage("You Win! Paper beats Rock.");
-//console.log("You Win! Paper beats Rock.")
-humanScore++;
+this.displayMessage("You Win! Paper beats Rock.");
+this.humanScore++;
 }
 else if(humanChoice == 'paper' && computerChoice == 'scissor'){
-displayMessage("You Lose! Scissor beats Paper.");
-//console.log("You Lose! Scissor beats Paper.")
-computerScore++;
+this.displayMessage("You Lose! Scissor beats Paper.");
+this.computerScore++;
 }
 else if(humanChoice == 'scissor' && computerChoice == 'rock'){
-displayMessage("You Lose! Rock beats Scissor.");
-//console.log("You Lose! Rock beats Scissor.")
-computerScore++;
+this.displayMessage("You Lose! Rock beats Scissor.");
+this.computerScore++;
 }
 else if(humanChoice == 'rock' && computerChoice == 'paper'){
+this.displayMessage("You Lose! paper beats Rock.");
+this.computerScore++;
+}
+else{
+     this.displayMessage("Play again")
+    };
 
-//console.log("You Lose! paper beats Rock.")
-displayMessage("You Lose! paper beats Rock.");
-computerScore++;
-}
-else{ displayMessage("Play again")};
-// console.log("Play again")}
 
-if (humanScore + computerScore === 5)
-{
-if(computerScore>humanScore)
-{
-//console.log
-finalresult("Winner is computer : " + computerScore );
+if (this.humanScore + this.computerScore === 5){
+       if(this.computerScore>this.humanScore){
+           this.finalresult("Winner is computer : " + this.computerScore );
+          }
+else if(this.computerScore<this.humanScore){ 
+this.finalresult("Winner is Human : " + this.humanScore);
 }
-else if(computerScore<humanScore)
-{ //console.log
-finalresult("Winner is Human : " + humanScore);
+else if(this.computerScore===this.humanScore){
+this.finalresult("Tie-Play again"); 
 }
-else if(computerScore===humanScore)
-{
-//console.log
-finalresult("Tie-Play again"); 
+this.resetgame();
 }
 
-resetgame();
-}
-resultMessage (`Current Score - Human: ${humanScore}, Computer: ${computerScore}`);
+this.resultMessage (`Current Score - Human: ${this.humanScore}, Computer: ${this.computerScore}`);
 
 }
+
+}
+new game();
+
+
